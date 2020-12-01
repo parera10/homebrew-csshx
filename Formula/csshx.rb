5b13cc412322dce671b3d9d3cc811c220d0de1b6
Guillem Parera <g.parera@avoristravel.com>
2020-12-01 23:57:19 +0100

    CLI  M Formula/csshx.rb -- This message was automatically generated by 'git auto'

--- Formula/csshx.rb
+++ Formula/csshx.rb
@@ -1,8 +1,8 @@
 class Csshx < Formula
   desc "Cluster ssh tool for Terminal.app"
   homepage "https://github.com/parera10/csshx"
-  url "https://github.com/parera10/csshx/archive/0.73-2.tar.gz"
-  sha256 "61c67d6c0fba6cbffecaad64bc547f40fa81e7151bf78a01d56b82029c1ac7bf"
+  url "https://github.com/parera10/csshx/archive/0.73-3.tar.gz"
+  sha256 "f17986100f7c469bde8bfe2073f02c7ffcf9dac1becb623e9e39d922d1f0874a"
   head "https://github.com/parera10/csshx.git"
 
   bottle :unneeded
